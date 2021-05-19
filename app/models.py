8bc8f4852e6927247be0d642ad8214fe951bd8a0
Manuel Garín Moriana <manuelgarin2@hotmail.com>
2021-05-19 23:54:50 +0200

    Add files via upload
    Corregido el problema con la base de datos y funcionando correctamente.

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 import os
 from sqlalchemy import Column
-from sqlalchemy.dialects.mysql import VARCHAR, Integer, String
+from sqlalchemy.dialects.mysql import VARCHAR, INTEGER, VARCHAR
 from app import db
 
 class Micotel(db.Model):
@@ -9,3 +9,6 @@ class Micotel(db.Model):
     mujer = Column(VARCHAR(2), nullable=True)
     hombre = Column(VARCHAR(2), nullable=True)
     infantil = Column(VARCHAR(2), nullable=True)
+
+    def __repr__(self):
+        return '<Micotel {}>'.format(self.email)
--- app/routes.py
+++ app/routes.py
@@ -2,12 +2,24 @@ from flask import render_template, request, Flask, redirect, url_for
 from app import app, db
 from app.models import Micotel
 from app.forms import LoginForm
+import requests
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'Micotel': Micotel}
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
     form=LoginForm()
     if form.validate_on_submit():
+        response=requests.get("https://emailvalidation.abstractapi.com/v1/?api_key=31f1d775cf294ae38d261fa83e65a103&email="+form.email.data)
+        print(response.status_code)
+        print(response.content)
+        # if  == True:
+        email=form.email.data
+        # else:
+        #     return redirect(url_for('index'))
         if form.mujer.data: 
             mujer = "Si"
         else: 
@@ -20,7 +32,7 @@ def index():
             infantil = "Si"
         else: 
             infantil = "No"
-        micotel = Micotel(email=form.email.data, mujer=mujer, hombre=hombre, infantil=infantil)
+        micotel = Micotel(email=email, mujer=mujer, hombre=hombre, infantil=infantil)
         try:
             db.session.add(micotel)
             db.session.commit()
