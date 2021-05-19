197ed8881b5f9849b25c15c68247cdf2fdf9630e
Manuel Garín Moriana <manuelgarin2@hotmail.com>
2021-05-19 21:36:45 +0200

    Add files via upload
    Añadido el código para la base de datos.

---
+++ app/models.py
@@ -0,0 +1,11 @@
+import os
+from sqlalchemy import Column
+from sqlalchemy.dialects.mysql import VARCHAR
+from app import db
+
+class Micotel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    mujer = Column(VARCHAR(2), nullable=True)
+    hombre = Column(VARCHAR(2), nullable=True)
+    infantil = Column(VARCHAR(2), nullable=True)
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template, request, Flask, redirect, url_for
-from app import app
+from app import app, db
+from app.models import Micotel
 from app.forms import LoginForm
 
 @app.route('/', methods=['GET', 'POST'])
@@ -7,10 +8,27 @@ from app.forms import LoginForm
 def index():
     form=LoginForm()
     if form.validate_on_submit():
-        print("datos enviados correctamente")
+        if form.mujer.data: 
+            mujer = "Si"
+        else: 
+            mujer = "No"
+        if form.hombre.data: 
+            hombre = "Si"
+        else: 
+            hombre = "No"
+        if form.infantil.data: 
+            infantil = "Si"
+        else: 
+            infantil = "No"
+        micotel = Micotel(email=form.email.data, mujer=mujer, hombre=hombre, infantil=infantil)
+        try:
+            db.session.add(micotel)
+            db.session.commit()
+        except:
+            db.session.rollback()
+            db.session.add(micotel)
+            db.session.commit()
         return redirect(url_for('micotel'))
-    else:
-        print("no")
     return render_template('index.html', form=form)
 
 @app.route('/micotel', methods=['GET', 'POST'])
